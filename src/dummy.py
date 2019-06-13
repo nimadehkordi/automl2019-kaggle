0f50a361c02976c66238844a2a1d494c33e15daa
Nima Riahi Dehkordi <nriahid@cmtcde6u018.rd.corpintra.net>
2019-06-13 15:45:31 +0200

    first run using dummy src

--- src/dummy.py
+++ src/dummy.py
@@ -6,15 +6,15 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.dummy import DummyRegressor
 
-# Input data files are available in the "../input/" directory.
+# Input data files are available in the "../data/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 import os
-print(os.listdir("../input"))
+print(os.listdir("/lhome/nriahid/Documents/automl2019-kaggle/data"))
 
 # load data
-df_train = pd.read_csv("../input/traindata.csv")
-df_test = pd.read_csv("../input/testdata.csv")
-y_train = pd.read_csv("../input/traindata_label.csv").values
+df_train = pd.read_csv("/lhome/nriahid/Documents/automl2019-kaggle/data/traindata.csv")
+df_test = pd.read_csv("/lhome/nriahid/Documents/automl2019-kaggle/data/testdata.csv")
+y_train = pd.read_csv("/lhome/nriahid/Documents/automl2019-kaggle/data/traindata_label.csv").values
 
 # Just take a subset of the available features
 x_train = df_train[["4046", "4225", "4770", "Total Bags", "Total Volume"]].values
@@ -26,4 +26,4 @@ model.fit(x_train, y_train)
 # Any results you write to the current directory are saved as output.
 train_pred = model.predict(x_train)
 test_pred = pd.DataFrame(model.predict(x_test))
-test_pred.to_csv("dummy_submitted_from_kernel.csv", header=["AveragePrice"], index_label="ID")
+test_pred.to_csv("/lhome/nriahid/Documents/automl2019-kaggle/data/dummy_submitted_from_kernel.csv", header=["AveragePrice"], index_label="ID")
